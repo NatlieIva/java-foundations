90238e098229a3b4548744ccbb143122e9b12dd9
natalieivanova <natalie_iva@icloud.com>
2020-10-01 15:26:26 +0300

    + quick sort

--- src/main/java/ru/itsjava/core/Recursion.java
+++ src/main/java/ru/itsjava/core/Recursion.java
@@ -1,11 +1,18 @@
 package ru.itsjava.core;
 
+import java.util.Arrays;
+
 public class Recursion {
 
     public static void main(String[] args) {
-        System.out.println(getFactorial(5));
-        printArray(4);
-        System.out.println(getFibonacciNumbers(6));
+//        System.out.println(getFactorial(5));
+//        printArray(4);
+//        System.out.println(getFibonacciNumbers(6));
+
+        int[] array = new int[]{3, 5, 1, 0, 6, 8, 2};
+        int lowMarker = 0;
+        int highMarker = array.length - 1;
+        System.out.println(Arrays.toString(quickSort(array, lowMarker, highMarker)));
     }
 
     public static int getFactorial(int num) {
@@ -34,5 +41,46 @@ public class Recursion {
             return getFibonacciNumbers(n - 1) + getFibonacciNumbers(n - 2);
 
         }
+    }
+
+    public static int[] quickSort(int[] array, int lowMarker, int highMarker) {
+        if (array.length == 0) {
+            System.out.println("Array is empty");
+            return array;
+        }
+        if (lowMarker >= highMarker) {
+            System.out.println("Incorrect boundaries ");
+            return array;
+        }
+        int midInd = lowMarker + (highMarker - lowMarker) / 2;
+        int midEl = array[midInd];
+
+        int i = lowMarker;
+        int j = highMarker;
+
+        while (i <= j) {
+            while (array[i] < midEl) {
+                i++;
+            }
+            while (midEl < array[j]) {
+                j--;
+            }
+            if (i <= j) {
+                int local = array[i];
+                array[i] = array[j];
+                array[j] = local;
+                i++;
+                j--;
+            }
+
+            if (lowMarker < j) {
+                quickSort(array, lowMarker, j);
+            }
+            if (i < highMarker) {
+                quickSort(array, i, highMarker);
+            }
+        }
+
+        return array;
     }
 }
