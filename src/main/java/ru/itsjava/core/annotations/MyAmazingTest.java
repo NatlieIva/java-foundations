9347498d414f90eccb955ab165be3a6a0486632f
natalieivanova <natalie_iva@icloud.com>
2021-02-15 18:28:18 +0300

    +My annotation

--- src/main/java/ru/itsjava/core/annotations/MyAmazingTest.java
+++ src/main/java/ru/itsjava/core/annotations/MyAmazingTest.java
@@ -2,50 +2,35 @@ package ru.itsjava.core.annotations;
 
 public class MyAmazingTest {
     @Before
-    public void test1Before() {
+    public void Before() {
+        System.out.println("Before");
     }
 
     @BeforeAll
-    public void test2BeforeAll() {
+    public void BeforeAll() {
+        System.out.println("Before All");
+    }
+
+    @After
+    public void After() {
+        System.out.println("After");
+    }
+
+    @AfterAll
+    public void AfterAll() {
+        System.out.println("After all");
+    }
+
+    @Test
+    public void test1() {
+    }
+
+    @Test
+    public void test2() {
     }
 
     @Test
     public void test3() {
-
-    }
-
-    @AfterAll
-    public void test4AfterAll() {
-    }
-
-    @BeforeAll
-    public void test5BeforeAll() {
-        ;
-    }
-
-    @Test
-    public void test6() {
-    }
-
-    @AfterAll
-    public void test7AfterAll() {
-
-    }
-
-    @BeforeAll
-    public void test8BeforeAll() {
-    }
-
-    @Test
-    public void test9() {
         throw new AssertionError();
-    }
-
-    @AfterAll
-    public void test10AfterAll() {
-    }
-
-    @After
-    public void test11After() {
     }
 }
--- src/main/java/ru/itsjava/core/annotations/MyJUnitRunner.java
+++ src/main/java/ru/itsjava/core/annotations/MyJUnitRunner.java
@@ -1,6 +1,7 @@
 package ru.itsjava.core.annotations;
 
 import java.lang.reflect.Method;
+import java.util.*;
 
 public class MyJUnitRunner {
 
@@ -17,16 +18,54 @@ public class MyJUnitRunner {
 
         MyAmazingTest myAmazingTest = new MyAmazingTest();
         Method[] declaredMethodsMyAmazingTest = MyAmazingTest.class.getDeclaredMethods();
+        List<Method> beforeQueue = new ArrayList<>();
+        List<Method> beforeAllQueue = new ArrayList<>();
+        List<Method> afterQueue = new ArrayList<>();
+        List<Method> afterAllQueue = new ArrayList<>();
+        List<Method> testQueue = new ArrayList<>();
 
         for (Method method : declaredMethodsMyAmazingTest) {
+            if (method.isAnnotationPresent(Before.class))
+                beforeQueue.add(method);
+            if (method.isAnnotationPresent(BeforeAll.class))
+                beforeAllQueue.add(method);
+            if (method.isAnnotationPresent(After.class))
+                afterQueue.add(method);
+            if (method.isAnnotationPresent(AfterAll.class))
+                afterAllQueue.add(method);
+            if (method.isAnnotationPresent(Test.class))
+                testQueue.add(method);
+        }
+
+        for (int i = 0; i < beforeQueue.size(); i++) {
             try {
-                method.invoke(myAmazingTest);
-                System.out.println("Test " + method.getName() + " successfully passed");
+                beforeQueue.get(i).invoke(myAmazingTest);
+            } catch (Throwable throwable) {
+            }
+        }
+        for (int i = 0; i < testQueue.size(); i++) {
+            try {
+                for (int j = 0; j < beforeAllQueue.size(); j++) {
+                    beforeAllQueue.get(j).invoke(myAmazingTest);
+                }
+                testQueue.get(i).invoke(myAmazingTest);
                 passedTests++;
+                System.out.println("Test " + testQueue.get(i).getName() + " successfully passed");
+
+                for (int j = 0; j < afterAllQueue.size(); j++) {
+                    afterAllQueue.get(j).invoke(myAmazingTest);
+                }
             } catch (Throwable throwable) {
                 failedTests++;
                 throwable.printStackTrace();
-                System.err.println("Test " + method.getName() + " failed");
+                System.err.println("Test " + testQueue.get(i).getName() + " failed");
+            }
+        }
+
+        for (int i = 0; i < beforeQueue.size(); i++) {
+            try {
+                beforeQueue.get(i).invoke(myAmazingTest);
+            } catch (Throwable throwable) {
             }
         }
 
